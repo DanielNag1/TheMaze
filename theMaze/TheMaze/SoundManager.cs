936dbd5976313e6241574d71b9c7fdd39a96f7b7
Zirko Miziri <zirko.miziri@live.com>
2019-05-02 01:21:45 +0200

    la in ett ljud
    gjorde en ofärdig sfx klass och la in ljudeffekt

---
+++ theMaze/TheMaze/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMaze
+{
+    public static class SoundManager
+    {
+        public static SoundEffect StepsSFX { get; private set; }
+        public static SoundEffect SafeRoomBGM { get; private set; }
+
+
+        public static void LoadContent(ContentManager content)
+        {
+            StepsSFX = content.Load<SoundEffect>("walking");
+
+
+        }
+
+    }
+}
