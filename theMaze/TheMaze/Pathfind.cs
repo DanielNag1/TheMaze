3f9c822cf9bf06d2c7d3c898755e77777707b3cb
Malin W <malin.wiren@hotmail.com>
2019-05-07 12:53:41 +0200

    Lagt till pathfinding
    Installerat Roy-T pathfinding bibliotek genom nuget-pack. Har skapat en statisk klass "Pathfind" som inehåller de metoder vi behöver till pathfindingen. Samt har jag skapat en tillfällig klass "PathfindMob" som har kod som berättar hur "pathfind"-klassen används i ett monster.

---
+++ theMaze/TheMaze/Pathfind.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoyT.AStar;
+
+namespace TheMaze
+{
+    static class Pathfind
+    {
+        private static Grid grid;
+
+        //Metod som skapar grid-objektet som pathfindingen använder sig av. 
+        //Behöver användas när man läser in kartan.
+        public static void FillGridFromMap(Tile[,] map)
+        {
+            int width = map.GetLength(0);
+            int height = map.GetLength(1);
+
+            grid = new Grid(width, height, 1.0f);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (map[x, y].IsWall)
+                    {
+                        grid.BlockCell(new Position(x, y));
+                    }
+                }
+            }
+        }
+
+        //Metod som returnerar "the path" i en lista med positionerna för tiles man ska vandra på. 
+        //Används av de monster som ska ha pathfinding. 
+        public static List<Vector2> CreatePath(Vector2 startPos, Vector2 endPos)
+        {
+            Position start = VectorToPosition(startPos);
+            Position end = VectorToPosition(endPos);
+
+            Position[] path = grid.GetPath(start, end, MovementPatterns.LateralOnly);
+            List<Vector2> pathPos = new List<Vector2>();
+
+            for (int i = 0; i < path.Length; i++)
+            {
+                pathPos.Add(PositionToVector(path[i]));
+            }
+
+            return pathPos;
+        }
+
+        //Converterar en Vektors position till ett Positions-objekt som pathfindingen använder sig av.
+        private static Position VectorToPosition(Vector2 position)
+        {
+            return new Position((int)(position.X / ConstantValues.tileWidth), (int)(position.Y / ConstantValues.tileHeight));
+        }
+
+        //Converterar från ett Positions-objekt till en Vektors position som spelet använder sig av. 
+        private static Vector2 PositionToVector(Position position)
+        {
+            return new Vector2(position.X * ConstantValues.tileWidth, position.Y * ConstantValues.tileHeight);
+        }
+
+        //Skapar en vektor med riktnigen mellan currentPos och nextPos för att kunna använda rörelse koden vi har.
+        public static Vector2 SetDirectionFromNextPosition(Vector2 currentPos, Vector2 nextPos)
+        {
+            int x = 0;
+            int y = 0;
+
+            if (currentPos.X < nextPos.X)
+            {
+                x = 1;
+            }
+            if (currentPos.X > nextPos.X)
+            {
+                x = -1;
+            }
+            if (currentPos.Y < nextPos.Y)
+            {
+                y = 1;
+            }
+            if (currentPos.Y > nextPos.Y)
+            {
+                y = -1;
+            }
+
+            return new Vector2(x, y);
+        }
+    }
+}
---
+++ theMaze/TheMaze/PathfindMob.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMaze
+{
+    class PathfindMob
+    {
+        public Vector2 Position { get; private set; }
+        private Texture2D texture;
+
+        LevelManager levelManager;
+
+        //En lista som håller "the path"
+        private List<Vector2> path;
+
+        private Vector2 direction, destination;
+
+        private float speed = 100;
+        private bool moving = false;
+
+        private float timer = 0f, resetTimer = 700f;
+
+
+        public PathfindMob(LevelManager levelManager, Vector2 startPosition)
+        {
+            Position = startPosition;
+            texture = TextureManager.CatTex;
+
+            this.levelManager = levelManager;
+
+            path = new List<Vector2>();
+        }
+
+        public void Update(GameTime gameTime, Player player)
+        {
+            //tid som räknar ner för att inte köra patfindingen för ofta
+            timer -= (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (timer < 0)
+            {
+                path = Pathfind.CreatePath(Position, player.Hitbox.Center.ToVector2());
+                timer = resetTimer;
+            }
+
+            Moving(gameTime);
+        }
+
+        private void Moving(GameTime gameTime)
+        {
+            if (!moving)
+            {
+                //koll så att listan med "the path" inte är tom för att motverka att programmet kraschar
+                if (path.Count != 0)
+                {
+                    //newDirection kallar på en metod i Pathfind som ger en vector där x och y antingen är 1 eller 0
+                    Vector2 newDirection = Pathfind.SetDirectionFromNextPosition(Position, path.First());
+                    //använder metoden som random movement också använder
+                    ChangeDirection(newDirection);
+                }
+            }
+
+            else
+            {
+                Position += direction * speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+                if (Vector2.Distance(Position, destination) < 1)
+                {
+                    Position = destination;
+                    moving = false;
+                    //kollar igen så att listan med "the path" inte är tom för att motverka krasch
+                    //tar sen bort första elementet i listan så att vi kan kalla på path.first() med nästa mål
+                    if (path.Count != 0)
+                    {
+                        path.RemoveAt(0);
+                    }
+                }
+            }
+        }
+
+        private void ChangeDirection(Vector2 newDirection)
+        {
+            direction = newDirection;
+            Vector2 newDestination = Position + direction * ConstantValues.tileWidth;
+
+            Tile tile = levelManager.GetTileAtPosition(direction);
+            if (tile.IsWall)
+            {
+                destination = newDestination;
+                moving = true;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Rectangle((int)Position.X, (int)Position.Y,
+                ConstantValues.tileWidth, ConstantValues.tileHeight), Color.White);
+        }
+    }
+}
