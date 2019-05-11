ad3abcbbc9c6fc4b4d33342420889a7ded96bb0c
Zirko Miziri <zirko.miziri@live.com>
2019-05-11 21:40:14 +0200

    Ersatte SFX klassen med den från V3.0-SFX
    Ersatte SFX klassen med SFX-klassen från senaste committen (as of 11/05/19) av Version-3.0-SFX.

--- theMaze/TheMaze/Sound/SFX.cs
+++ theMaze/TheMaze/Sound/SFX.cs
@@ -10,52 +10,43 @@ namespace TheMaze
 {
     class SFX
     {
-        enum PlayState { Step1, Step2 }
-        PlayState playState;
+        private enum PlayState { Step1, Step2 }
+        private PlayState currentPlayState;
 
-        private SoundEffectInstance step1;
-        private SoundEffectInstance step2;
-
-        double stepTimer;
-        bool startStepTimer, playStep1, playStep2;
+        private double stepTimer;
+        private bool playStep1, playStep2;
 
         public SFX()
         {
-            playState = PlayState.Step1;
+            currentPlayState = PlayState.Step1;
 
-            step1 = SoundManager.step1.CreateInstance();
-            step2 = SoundManager.step2.CreateInstance();
+            stepTimer = 700;
 
-            stepTimer = 1000;
-
-            startStepTimer = false;
             playStep1 = true;
             playStep2 = false;
         }
 
         public void Footsteps(GameTime gameTime)
         {
-            switch (playState)                 //efter andra steget så tar det tre sekunder att spela upp första steget igen
+            switch (currentPlayState)                 //efter andra steget så tar det tre sekunder att spela upp första steget igen
             {
                 case PlayState.Step1:
                     {
                         if (playStep1)
                         {
-                            step1.Play();
+                            stepTimer = 700;
 
-                            playStep1 = false;
-                            startStepTimer = true;
+                            SoundManager.step1.Play();
+
+                            playStep1 = false;   
                         }
-
-                        if (startStepTimer)
+                        else
                         {
                             stepTimer -= gameTime.ElapsedGameTime.TotalMilliseconds;
                             if (stepTimer <= 0)
                             {
-                                startStepTimer = false;
-                                stepTimer = 1000;
                                 playStep2 = true;
-                                playState = PlayState.Step2;
+                                currentPlayState = PlayState.Step2;
                             }
                         }
 
@@ -65,21 +56,19 @@ namespace TheMaze
                     {
                         if (playStep2)
                         {
-                            step2.Play();
+                            stepTimer = 700;
+
+                            SoundManager.step2.Play();
 
                             playStep2 = false;
-                            startStepTimer = true;
                         }
-
-                        if (startStepTimer)
+                        else
                         {
                             stepTimer -= gameTime.ElapsedGameTime.TotalMilliseconds;
                             if (stepTimer <= 0)
                             {
-                                startStepTimer = false;
-                                stepTimer = 1000;
                                 playStep1 = true;
-                                playState = PlayState.Step1;
+                                currentPlayState = PlayState.Step1;
                             }
                         }
 
